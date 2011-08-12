b6672601c87a40c47aee9cccd3c4ecbb3048a72c
glaice <glaice.10@gmail.com>
2011-08-12 01:45:25 -0300

    mudanca no layout padronizando a classe noticia.

--- index.php
+++ index.php
@@ -11,57 +11,18 @@ include "cabecalho.php";
 include "pos_cabecalho.php";
 
 ?>
-	<div id="noticia"><h3>6º Retiro Espiritual de Carnaval GASPE</h3>
-        <img src="http://farm6.static.flickr.com/5134/5558666389_6b3acb50c5_m.jpg">
-        <p> O 6º Retiro Espiritual de Carnaval do grupo, da Matriz São José Operário, da Paróquia Santa Teresinha de Jesus. Foram quatro dias inesquecíveis! O Encontro teve início às 17:00h do dia 5 de março, com a entrega de crachás e copos, divisão dos grupos da gincana e momentos emocionantes, tanto no enduro à pé noturno, quanto na adoração que finalizou o dia. O local do encontro, o Seminário Diocesano de Marilândia, proporcionou uma estrutura confortável para os retirandos, de paz, descanso, com uma vista linda. Esse momento foi marcante na vida dos participantes. Com o tema:<b>"Buscai as coisas do alto"</b> e o lema: "Jovem não te detenhas pois o Senhor está contigo", foi proporcionado vários meios de aproximação com a vida que leva para a verdadeira felicidade.<br> <a href="fotosRetiro2011.php">Confira as fotos do encontro!</a></p>
+	<div id="noticia"><a href="fotosRetiro2011.php"><h3>6º Retiro Espiritual de Carnaval GASPE</h3></a>
+        <a href="fotosRetiro2011.php"> <img class="home" src="http://farm6.static.flickr.com/5134/5558666389_6b3acb50c5_m.jpg"></a>
+        <p class="home"> O 6º Retiro Espiritual de Carnaval do grupo, da Matriz São José Operário, da Paróquia Santa Teresinha de Jesus. Foram quatro dias inesquecíveis! O Encontro teve início às 17:00h do dia 5 de março, com a entrega de crachás e copos, divisão dos grupos da gincana e momentos emocionantes, tanto no enduro à pé noturno, quanto na adoração que finalizou o dia. O local do encontro, o Seminário Diocesano de Marilândia, proporcionou uma estrutura confortável para os retirandos, de paz, descanso, com uma vista linda. Esse momento foi marcante na vida dos participantes. Com o tema:<b>"Buscai as coisas do alto"</b> e o lema: "Jovem não te detenhas pois o Senhor está contigo", foi proporcionado vários meios de aproximação com a vida que leva para a verdadeira felicidade.<br>
+		 <a class="link" href="fotosRetiro2011.php"> Confira as fotos do encontro!</a>
+		 </p>
 	</div>
 
-	<div id="noticia"><h3>Venha participar do GASPE</h3>
-        <img src="images/participar.png">
-        <p> O GASPE (Grupo de Amigos Semeando a Paz e o Evangelho) convida a todos a visitarem o grupo, nos sábados as 17:00h, na matriz São José Operário, da Paróquia Santa Teresinha do Menino Jesus. Não percam a chance de conhecer esse grupo, que a cada ano vem fortalecendo seus laços de amizade e sua caminhada cristã. O grupo, além de cantar na matriz, realiza eventos durante o ano que fortalecem a espiritualidade dos jovens. Acolheremos a todos de braços abertos!<br><a href="mensagem.php">Leia mais...</a></p>
+	<div id="noticia"><a href="mensagem.php"><h3>Venha participar do GASPE</h3></a>
+        <a href="mensagem.php"><img class="home" src="images/participar.png"></a>
+        <p class="home"> O GASPE (Grupo de Amigos Semeando a Paz e o Evangelho) convida a todos a visitarem o grupo, nos sábados as 17:00h, na matriz São José Operário, da Paróquia Santa Teresinha do Menino Jesus. Não percam a chance de conhecer esse grupo, que a cada ano vem fortalecendo seus laços de amizade e sua caminhada cristã. O grupo, além de cantar na matriz, realiza eventos durante o ano que fortalecem a espiritualidade dos jovens. Acolheremos a todos de braços abertos!<br>
+        <a class="link" href="mensagem.php">Leia mais...</a></p>
 	</div>
-
-
-<p style="font-size:14px;"> Em todas as realizações do grupo temos o... <br><br></p>
-  <h3> Apoio:</h3>
-                <ul>
-                    <li> Paróquia Santa Teresinha do Menino Jesus</li>
-                    <li> Comunidade São José Operário</li>
-                </ul>
-<p style="font-size:14px;"><br>...e nesse ano, tivemos a ajuda de patrocinadores e colaboradores no nosso retiro de canaval:</p>
-
-        <p style="font-size:16px;"><br><b> Conheça os patrocinadores do nosso retiro!</b>
-
-
-		<div id="area-patrocinio">
-		        <img src="images/patrocinador/1.png">
-		        <img src="images/patrocinador/2.png">
-		        <img src="images/patrocinador/3.jpg">
-		        <img src="images/patrocinador/4.jpg">
-		        <img src="images/patrocinador/5.jpg">
-		        <img src="images/patrocinador/6.jpg">
-		        <img src="images/patrocinador/7.jpg">
-		        <img src="images/patrocinador/8.jpg">
-		        <img src="images/patrocinador/9.jpg">
-		        <img src="images/patrocinador/10.jpg">
-		        <img src="images/patrocinador/11.jpg">
-		        <img src="images/patrocinador/12.png">
-		        <img src="images/patrocinador/13.png">
-		        <img src="images/patrocinador/14.jpg">
-		</div>
-
-		<p style="font-size:16px;"><br><b> Conheça os colaboradores do nosso retiro!</b>
-
-		<div id="area-patrocinio">
-	      		<img style="width:405px; height: 120px;" src="images/colaborador/C3.png">
-		        <img src="images/colaborador/C2.jpg">
-		        <img style="clear:left" src="images/colaborador/C1.JPG">
-		        <img src="images/colaborador/C4.png"> 
-		        <img src="images/colaborador/C5.png"> 
-		        <img src="images/colaborador/C6.png"> 
-		        <img src="images/colaborador/C7.png"> 
-		        <img src="images/colaborador/C8.png">
-		</div>
 
 
 <?php

c8205cd78d2254b2d8b74da4b0876780b30c9511
Glaice Kelly < glaice.10@gmail.com>
2011-11-01 00:40:05 -0200

    adicionei paginas

---
+++ fotosBarraTriunfo.php
@@ -0,0 +1,67 @@
+<?php
+
+include "cabecalho.php";
+
+?>
+    <title> Fotos de Barra do Triunfo </title>
+
+<?php
+
+include "pos_cabecalho.php";
+
+?>
+
+
+<center>
+<div style="width: 80%">
+
+    <h2>Barra do Triunfo</h2>
+</div>
+
+<!Criando a tabela de fotos:>
+
+    <div class="content">
+        <div id="galleria">
+             
+            <img src="http://farm7.static.flickr.com/6114/6300523796_c6b37f48a6.jpg">
+            <img src="http://farm7.static.flickr.com/6046/6300524932_634d1fa5ea.jpg">
+            <img src="http://farm7.static.flickr.com/6057/6300524660_fe294e361c.jpg">
+            <img src="http://farm7.static.flickr.com/6109/6300524430_2a2fcfc636.jpg">
+            <img src="http://farm7.static.flickr.com/6059/6300523688_42f783fce5.jpg">
+            <img src="http://farm7.static.flickr.com/6032/6299990455_87405733ec.jpg">
+            <img src="http://farm7.static.flickr.com/6048/6300524258_81d976f495.jpg">
+            <img src="http://farm7.static.flickr.com/6211/6300525408_3b916b803c.jpg">
+            <img src="http://farm7.static.flickr.com/6116/6300525270_0250692c4c.jpg">
+            <img src="http://farm7.static.flickr.com/6214/6300525176_fea41cd4ec.jpg">
+            <img src="http://farm7.static.flickr.com/6044/6299991633_7b6a65ac9b.jpg">
+            <img src=" http://farm7.static.flickr.com/6230/6299990943_6110b8f5d3.jpg">
+            <img src="http://farm7.static.flickr.com/6042/6299990803_ee3e05d6f8.jpg">
+            <img src="http://farm7.static.flickr.com/6093/6300288417_c56a9bfc65.jpg">
+            <img src="http://farm7.static.flickr.com/6232/6300821490_559ec61d84.jpg">
+            <img src="http://farm7.static.flickr.com/6239/6300821426_e6584221b1.jpg">
+            <img src="http://farm7.static.flickr.com/6232/6300821286_c00fd2f1a9.jpg">
+            <img src="http://farm7.static.flickr.com/6115/6299990341_94637e1f8a.jpg">
+            <img src="http://farm7.static.flickr.com/6098/6300822496_7e8eded35a.jpg">
+            <img src="http://farm7.static.flickr.com/6114/6300289231_f01e940fe2.jpg">
+            <img src="http://farm7.static.flickr.com/6039/6300822234_0816ee4133.jpg">
+            <img src="http://farm7.static.flickr.com/6038/6300822014_b11ddb6fdc.jpg">
+            <img src="http://farm7.static.flickr.com/6217/6300288517_0b2465ee54.jpg">
+            <img src="http://farm7.static.flickr.com/6051/6300821858_aa6301d833.jpg">
+        </div>
+        
+    </div>
+    <script>
+    
+    // Load the classic theme
+    Galleria.loadTheme('paradaprafotos/galleria.classic.js');
+
+    // Initialize Galleria
+    $('#galleria').galleria();
+    
+    </script>
+
+<?php
+
+include "rodape.php";
+
+?>

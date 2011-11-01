bb445f52d0b3551c64929c1a23e3a739ee5d9970
Glaice Kelly <glaice.10@gmail.com>
2011-11-01 00:40:05 -0200

    adicionei paginas

---
+++ fotosDespedida.php
@@ -0,0 +1,65 @@
+<?php
+
+include "cabecalho.php";
+
+?>
+    <title> Despedida do Pe. Mauricio </title>
+
+<?php
+
+include "pos_cabecalho.php";
+
+?>
+
+
+<center>
+<div style="width: 80%">
+
+    <h2> Despedida do Pe. Maurício</h2>
+</div>
+
+<!Criando a tabela de fotos:>
+
+    <div class="content">
+        <div id="galleria">
+             
+            <img src="http://farm5.static.flickr.com/4112/5091578073_1dd50a9fc1.jpg">
+            <img src="http://farm5.static.flickr.com/4145/5091583443_2822dfde6d.jpg">
+            <img src="http://farm5.static.flickr.com/4152/5092183000_6fe879d1bd.jpg">
+            <img src="http://farm5.static.flickr.com/4111/5091582747_06d1192094.jpg">
+            <img src="http://farm5.static.flickr.com/4088/5092179858_e4137d75c5.jpg">
+            <img src="http://farm5.static.flickr.com/4088/5092187080_c13faca1f7.jpg">
+            <img src="http://farm5.static.flickr.com/4091/5092184176_aaff202254.jpg">
+            <img src="http://farm5.static.flickr.com/4089/5092421857_99df317198.jpg">
+            <img src="http://farm5.static.flickr.com/4083/5091581957_b73acd0337.jpg">
+            <img src="http://farm5.static.flickr.com/4089/5091585201_bd2cb55084.jpg">
+
+            <img src="http://farm5.static.flickr.com/4103/5092421715_a744eeb527.jpg">
+            <img src="http://farm5.static.flickr.com/4111/5093019522_7ba894d4a0.jpg">
+            <img src="http://farm5.static.flickr.com/4133/5091586889_957f792b0c.jpg">
+            <img src="http://farm5.static.flickr.com/4131/5092178920_eeab393684.jpg">
+            <img src="http://farm5.static.flickr.com/4112/5092422069_8c68eb6154.jpg">
+            <img src="http://farm5.static.flickr.com/4130/5091649873_d04c76aa3e.jpg">
+            <img src="http://farm5.static.flickr.com/4152/5092421587_88e6397e6c.jpg">
+            <img src="http://farm5.static.flickr.com/4124/5091586753_676c7c855e.jpg">
+            <img src="http://farm5.static.flickr.com/4088/5092183266_e47e17b993.jpg">
+            <img src="http://farm5.static.flickr.com/4107/5091588041_9725654579.jpg">
+
+        </div>
+        
+    </div>
+    <script>
+    
+    // Load the classic theme
+    Galleria.loadTheme('paradaprafotos/galleria.classic.js');
+
+    // Initialize Galleria
+    $('#galleria').galleria();
+    
+    </script>
+
+<?php
+
+include "rodape.php";
+
+?>
---
+++ fotosProjetoSC.php
@@ -0,0 +1,78 @@
+<?php
+
+include "cabecalho.php";
+
+?>
+    <title> Fotos do Projeto Santa Clara - Ponta da Fruta </title>
+
+<?php
+
+include "pos_cabecalho.php";
+
+?>
+
+
+<center>
+<div style="width: 80%">
+
+    <h2>Projeto Santa Clara</h2>
+</div>
+
+<!Criando a tabela de fotos:>
+
+    <div class="content">
+        <div id="galleria">
+             
+            <img src="http://farm7.static.flickr.com/6052/6299961917_b8eed42678.jpg">
+            <img src="http://farm7.static.flickr.com/6230/6300498412_5d08cf789b.jpg">
+            <img src="http://farm7.static.flickr.com/6053/6299957303_9be1b128cb.jpg">
+            <img src="http://farm7.static.flickr.com/6043/6299956565_5bf99e3557.jpg">
+            <img src="http://farm7.static.flickr.com/6048/6299959379_5acc44e828.jpg">
+            <img src="http://farm7.static.flickr.com/6044/6300484888_f4a66bc173.jpg">
+            <img src="http://farm7.static.flickr.com/6231/6299949561_83268f7c3e.jpg">
+            <img src="http://farm7.static.flickr.com/6118/6299936985_be251d2c1d.jpg">
+            <img src="http://farm7.static.flickr.com/6239/6300478544_5b7ef0f9db.jpg">
+            <img src="http://farm7.static.flickr.com/6217/6299937861_230fb019f2.jpg">
+            <img src="http://farm7.static.flickr.com/6106/6300480180_ba41cf26fd.jpg">
+            <img src="http://farm7.static.flickr.com/6043/6299942287_ff5e66970d.jpg">
+            <img src="http://farm7.static.flickr.com/6056/6300489776_77af09265f.jpg"> 
+            <img src="http://farm7.static.flickr.com/6230/6299952765_31e2bb9421.jpg">
+            <img src="http://farm7.static.flickr.com/6109/6299953631_ffc142a129.jpg">
+            <img src="http://farm7.static.flickr.com/6111/6299947625_e053fc5ac4.jpg">
+            <img src="http://farm7.static.flickr.com/6228/6300485404_fbe882a07e.jpg">
+            <img src="http://farm7.static.flickr.com/6235/6299943669_e1157c8d09.jpg">
+            <img src="http://farm7.static.flickr.com/6092/6299932213_758e806250.jpg">
+            <img src="http://farm7.static.flickr.com/6039/6299931659_2dc673a233.jpg">
+            <img src="http://farm7.static.flickr.com/6059/6299930745_b5723eef31.jpg">
+            <img src="http://farm7.static.flickr.com/6223/6299928929_8ce1e8d3dc.jpg">
+            <img src="http://farm7.static.flickr.com/6056/6299925229_244435ce7c.jpg">
+            <img src="http://farm7.static.flickr.com/6058/6299924387_be03667ed2.jpg">
+            <img src="http://farm7.static.flickr.com/6057/6299922787_bee9f96ba8.jpg">
+            <img src="http://farm7.static.flickr.com/6239/6300461196_7d87bc6f6a.jpg">
+            <img src="http://farm7.static.flickr.com/6101/6300453178_84fb3bab67.jpg">
+            <img src="http://farm7.static.flickr.com/6057/6300497402_64ecd63ac4.jpg">
+            <img src="http://farm7.static.flickr.com/6059/6300453700_2eaa7d22d3.jpg">
+            <img src="http://farm7.static.flickr.com/6103/6299962677_d00c871a6d.jpg">
+            <img src="http://farm7.static.flickr.com/6236/6300454438_a45b02d59e.jpg">
+            <img src="http://farm7.static.flickr.com/6037/6299961399_530c5eb9e9.jpg">
+        </div>
+        
+    </div>
+    <script>
+    
+    // Load the classic theme
+    Galleria.loadTheme('paradaprafotos/galleria.classic.js');
+
+    // Initialize Galleria
+    $('#galleria').galleria();
+    
+    </script>
+
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=d&amp;source=s_d&amp;saddr=Aracruz+-+ES&amp;daddr=Ponta+da+Fruta,+Vila+Velha+-+ES&amp;hl=pt-BR&amp;geocode=FcaT0f4dW3aZ_SkdnYwdr9y3ADEtUCzBkBcmkw%3BFTmcx_4dAlWY_SkXyRU_qEG4ADGzJQ5oKd7ZjA&amp;aq=t&amp;sll=-14.239424,-53.186502&amp;sspn=62.974459,135.263672&amp;vpsrc=6&amp;mra=ls&amp;ie=UTF8&amp;ll=-20.08044,-40.365143&amp;spn=0.66722,0.15175&amp;t=m&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com.br/maps?f=d&amp;source=embed&amp;saddr=Aracruz+-+ES&amp;daddr=Ponta+da+Fruta,+Vila+Velha+-+ES&amp;hl=pt-BR&amp;geocode=FcaT0f4dW3aZ_SkdnYwdr9y3ADEtUCzBkBcmkw%3BFTmcx_4dAlWY_SkXyRU_qEG4ADGzJQ5oKd7ZjA&amp;aq=t&amp;sll=-14.239424,-53.186502&amp;sspn=62.974459,135.263672&amp;vpsrc=6&amp;mra=ls&amp;ie=UTF8&amp;ll=-20.08044,-40.365143&amp;spn=0.66722,0.15175&amp;t=m" style="color:#0000FF;text-align:left">Exibir mapa ampliado</a></small>
+
+
+<?php
+
+include "rodape.php";
+
+?>
